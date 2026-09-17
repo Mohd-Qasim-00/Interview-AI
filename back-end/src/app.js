e7c9c56c9ce47c3230d40a5beb90c5834e4cef16
qasim <qasimshaikh.0068@gmail.com>
2026-09-17 19:15:52 +0530

    Second commit

--- back-end/src/app.js
+++ back-end/src/app.js
@@ -18,7 +18,7 @@ app.use((req, res, next) => {
   next();
 });
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: "interview-ai-kappa-dun.vercel.app",
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
 
